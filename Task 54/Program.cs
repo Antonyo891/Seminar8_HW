70df2c9891b7783c08ff19fd10c1cdd0354536a2
Anton <B_atonyo_S@bk.ru>
2023-06-18 09:12:51 +0300

    TAsk 54 completed

---
+++ Task 54/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.*/
+using System;
+using static System.Console;
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($" {array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+int[,] GetArray(int line, int column) // создает двумерный массив с рандомными элементами
+{
+    int[,] result = new int[line, column];
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < column; j++) result[i, j] = new Random().Next(10);
+    }
+    return result;
+}
+void SortingLineOfArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            for (int k = array.GetLength(1) - 1; k > j; k--)
+            {
+                if (array[i, k] > array[i,k - 1])
+                {
+                    int save = array[i,k];
+                    array[i,k] = array[i,k - 1];
+                    array[i,k - 1] = save;
+                }
+            }
+}
+Clear();
+int[,] array = GetArray(10, 10);
+PrintArray(array);
+SortingLineOfArray(array);
+WriteLine();
+ReadLine();
+PrintArray(array);
